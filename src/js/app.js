7ec07ee54792b9607407baf1dd45fa136880d56c
CT-1337 <65622269+CT-1337@users.noreply.github.com>
2024-02-17 21:36:17 -0600

    initial push

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
 global.$ = require('jquery');
 require('bootstrap');
+require('../../src/vendor/slick-1.8.1/slick.js');
 
 $( document ).ready(function() {
 
+    console.log('Test Start');
+
+    console.log('Test End');
 
 });
